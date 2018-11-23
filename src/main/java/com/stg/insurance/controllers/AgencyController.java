f7c29c9b9a1179082b2863c01ae4dd8634510c3b
Aditya <av_adi90@yahoo.com>
2018-11-23 22:59:31 +0530

    Updated RestControllers

--- src/main/java/com/stg/insurance/controllers/AgencyController.java
+++ src/main/java/com/stg/insurance/controllers/AgencyController.java
@@ -7,11 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author kinjaldoshi
  *
  */
+@RestController
 public class AgencyController {
 	
 	@GetMapping ("/ediPlatform/getAllAgencies")
